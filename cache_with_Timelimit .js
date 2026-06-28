d2d9e3458ca1c7cba2d5460a6d68f3723ba194d6
rutvik <rutvikpatil3896@gmail.com>
2026-06-28 18:47:42 +0530

    added new file

---
+++ cache_with_Timelimit .js
@@ -0,0 +1,44 @@
+var TimeLimitedCache = function () {
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration
+ * @return {boolean}
+ */
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+    const exists = this.cache.has(key);
+
+    // If key already exists, cancel its previous expiration timer
+    if (exists) {
+        clearTimeout(this.cache.get(key).timeoutId);
+    }
+
+    // Create a new expiration timer
+    const timeoutId = setTimeout(() => {
+        this.cache.delete(key);
+    }, duration);
+
+    this.cache.set(key, { value, timeoutId });
+
+    return exists;
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+TimeLimitedCache.prototype.get = function (key) {
+    if (!this.cache.has(key)) return -1;
+
+    return this.cache.get(key).value;
+};
+
+/**
+ * @return {number}
+ */
+TimeLimitedCache.prototype.count = function () {
+    return this.cache.size;
+};
